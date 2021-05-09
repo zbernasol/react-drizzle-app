8983d7ae9320848de308d6307697d75bd5b121aa
Zian Bernasol <77581628+zbernasol@users.noreply.github.com>
2021-05-09 14:07:55 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Drizzle</h1>
-      <footer>
-         
-        <a href="https://github.com/zbernasol/react-drizzle-app" target="_blank" >
-          Open-source
-        </a> code by <a href="https://www.linkedin.com/in/zian-angelica-bernasol-6a190a113/" target="_blank">Zian Bernasol</a>
-      </footer>
-      
+      <div className="container">
+        <h1>Drizzle</h1>
+        <footer>
+          
+          <a href="https://github.com/zbernasol/react-drizzle-app" target="_blank" >
+            Open-source
+          </a> code by <a href="https://www.linkedin.com/in/zian-angelica-bernasol-6a190a113/" target="_blank">Zian Bernasol</a>
+        </footer>
+      </div>
     </div>
   );
 }
