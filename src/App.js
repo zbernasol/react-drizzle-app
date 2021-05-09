ec7ff23a9fa1725cdbd01de70753aef70c791e74
Zian Bernasol <77581628+zbernasol@users.noreply.github.com>
2021-05-09 13:52:18 -0400

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Drizzle
+      <h1>Drizzle</h1>
+      <footer>
+         
+        <a href="https://github.com/zbernasol/react-drizzle-app" target="_blank" >
+          Open-source
+        </a> code by <a href="https://www.linkedin.com/in/zian-angelica-bernasol-6a190a113/" target="_blank">Zian Bernasol</a>
+      </footer>
+      
     </div>
   );
 }
